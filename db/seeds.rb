49f8f7eb24eadad9fb7545a63fe78d5ef7a355e3
sheen <ysmsmno1@gmail.com>
2015-05-17 14:37:55 +1000

    add location setup method in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Location.import_location_data if Location.all.blank?
