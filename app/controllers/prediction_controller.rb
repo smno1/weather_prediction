3bb22d3aee4c86a26deaad8ec2b4cc2da76290c9
sunshiness <yangguangshi@YangguangShideMacBook-Pro.local>
2015-05-26 16:13:19 +1000

    get data from database, processing and do prediction

--- app/controllers/prediction_controller.rb
+++ app/controllers/prediction_controller.rb
@@ -15,6 +15,7 @@ class PredictionController < ApplicationController
     dt-=1.day
 
     x_data = []
+    x_formated_time = []
     temp_y_data = []
     rain_y_data = []
     wind_dir_y_data = []
@@ -26,34 +27,34 @@ class PredictionController < ApplicationController
 
     wdrecs=WeatherDataRecording.where(:recording_time=>dt.at_beginning_of_day..dt.at_end_of_day, :station_id=>@station.id)
     wdrecs.each do |rec|
-      rec.recording_time
+      x_data = rec.recording_time
       temp_y_data = rec.temperature_record.cel_degree
     end
 
     wdrecs=WeatherDataRecording.where(:recording_time=>dt.at_beginning_of_day..dt.at_end_of_day, :station_id=>@station.id)
     wdrecs.each do |rec|
-      rec.recording_time
       rain_y_data = rec.rain_fall_records.precip_amount
     end
 
     wdrecs=WeatherDataRecording.where(:recording_time=>dt.at_beginning_of_day..dt.at_end_of_day, :station_id=>@station.id)
     wdrecs.each do |rec|
-      rec.recording_time
       wind_dir_y_data = rec.wind_records.win_dir
     end
 
     wdrecs=WeatherDataRecording.where(:recording_time=>dt.at_beginning_of_day..dt.at_end_of_day, :station_id=>@station.id)
     wdrecs.each do |rec|
-      rec.recording_time
       wind_speed_y_data = rec.wind_records.win_speed
+    end
+
+    x_data.each do |time|
+      x_formated_time = time.hour + time.min/60
     end
 
     preditUtil = PredictionUtil.new
 
-    predict_temp = preditUtil.prediction(@station.name, x_data, temp_y_data, @period_toi)
-    predict_rain = preditUtil.prediction(@station.name, x_data, rain_y_data, @period_toi)
-    predict_win_dir = preditUtil.prediction(@station.name, x_data, predict_win_dir, @period_toi)
-    predict_wind_speed = preditUtil.prediction(@station.name, x_data, predict_wind_speed, @period_toi)
-    
+    predict_temp = preditUtil.prediction(@station.name, x_formated_time, temp_y_data, @period_toi)
+    predict_rain = preditUtil.prediction(@station.name, x_formated_time, rain_y_data, @period_toi)
+    predict_win_dir = preditUtil.prediction(@station.name, x_formated_time, predict_win_dir, @period_toi)
+    predict_wind_speed = preditUtil.prediction(@station.name, x_formated_time, predict_wind_speed, @period_toi)
   end
 end
